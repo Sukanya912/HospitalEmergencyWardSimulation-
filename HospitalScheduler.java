f4ce3a9698f0dfcdc9f4868189f0f74eb387ce3c
Sukanya Sinha <sukanyasinha5@gmail.com>
2023-02-13 00:04:09 +0530

    Update HospitalScheduler.java
    Updated with comments

--- HospitalScheduler.java
+++ HospitalScheduler.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 /*
-// severity: P0 -> Needs immediate attention, P1 -> Not very critical, Can wait
-// timeTaken: in minutes
+// `Patient` class
+// `patientNumber`: An integer representing the patient number.
+// `severity`: An integer representing the patient's severity, where 0 represents critical and 1 represents non-critical.
+// `timeTaken`: An integer representing the time taken by the doctor to attend the patient.
 */
 
 class Patient{
@@ -18,8 +20,13 @@ class Patient{
 }
  
 /*
-// HospitalScheduler class
-// doctorAttendingPatient: default null, means not attending
+// `HospitalScheduler` class
+// `numberOfDoctors`: An integer representing the number of doctors in the hospital.
+// `doctorAttendingPatient`: An array of Patient objects, representing the patients that are being attended by each doctor.
+// `patientsQueue`: A priority queue of Patient objects, representing the patients waiting to be attended in the order of their severity and patient number.
+// `patientArrived`: A set of integers representing the patient numbers of patients who have already arrived at the hospital.
+// `criticalPatients`: An integer representing the number of critical patients waiting in the queue.
+// `nonCriticalPatients`: An integer representing the number of non-critical patients waiting in the queue.
 */
 class HospitalScheduler{
  
@@ -44,13 +51,13 @@ class HospitalScheduler{
         this.nonCriticalPatients = 0;
     }
  
-    // adds new patient
+    // `addNewPatient`: Adds a new patient to the hospital and updates the waiting queue and patient list.
     void addNewPatient(Patient patient){
         if(patientArrived.contains(patient.patientNumber)){
             System.out.println("Patient: #" +patient.patientNumber + " Already Taken into Consideration");
             return;
         }
-        if(patient.severity==0){
+        if(patient.severity == 0){
             if(criticalPatients>0){
                 System.out.println("Please wait, you are at waiting number #" + (criticalPatients + 1));
             }
@@ -58,7 +65,7 @@ class HospitalScheduler{
             criticalPatients++;
         }
         else{
-            if(patientsQueue.size() > 0 || (findDoctorAccToSeverity(-1)==-1)){
+            if(patientsQueue.size() > 0 || (findDoctorAccToSeverity(-1) == -1)){
                 System.out.println("Patient: #"+ patient.patientNumber + ", Please wait, you are at waiting number #" + (patientsQueue.size() + 1));
             }
             patientsQueue.add(patient);
@@ -68,10 +75,10 @@ class HospitalScheduler{
         visitDoctor();
     }
  
-    // visit Doctor
+    // `visitDoctor`: Assigns the next patient in the queue to a doctor.
     void visitDoctor(){
         Patient patient = patientsQueue.poll();
-        if(patient.severity==0){
+        if(patient.severity == 0){
             int potentialDoctor = findNextAvailableDoctor(patient);
             assignDoctor(patient, potentialDoctor);
             criticalPatients--;
@@ -83,28 +90,28 @@ class HospitalScheduler{
         }
     }
  
-    // assign Doctor
+    // `assignDoctor`: Assigns a patient to a specific doctor.
     void assignDoctor(Patient patient, int doctor){
-        if(patient.severity == 0 && doctorAttendingPatient[doctor]!=null && doctorAttendingPatient[doctor].severity==1){
+        if(patient.severity == 0 && doctorAttendingPatient[doctor] != null && doctorAttendingPatient[doctor].severity == 1){
             patientsQueue.add(doctorAttendingPatient[doctor]);
             System.out.println("Critical patient, Doctor: #" + (doctor+1) + " is now attending Patient: #" + patient.patientNumber);
             visitDoctor();
         }
         else{
-            System.out.println("Doctor: #" + (doctor+1) + " is attending Patient: #" + patient.patientNumber);
+            System.out.println("Doctor: #" + (doctor + 1) + " is attending Patient: #" + patient.patientNumber);
         }
         doctorAttendingPatient[doctor] = patient;
     }
     
-    // Find Next available doctor
+    // `findNextAvailableDoctor`: Finds the next available doctor based on the patient's severity.
     int findNextAvailableDoctor(Patient patient){
         int potentialDoctor = findDoctorAccToSeverity(-1);
-        if(potentialDoctor!=-1){
+        if(potentialDoctor != -1){
             return potentialDoctor;
         }
-        if(patient.severity==0){
+        if(patient.severity == 0){
             potentialDoctor = findDoctorAccToSeverity(1);
-            if(potentialDoctor!=-1){
+            if(potentialDoctor != -1){
                 return potentialDoctor;
             }
         }
@@ -113,11 +120,11 @@ class HospitalScheduler{
         return potentialDoctor;
     }
  
-    //utility for Find doctor according to Severity
+    //`findDoctorAccToSeverity`: A utility method that finds a doctor with a specific severity, if available.
     int findDoctorAccToSeverity(int severity){
         int potentialDoctor = -1, waitTime =  Integer.MAX_VALUE;
         for(int doctor = 0; doctor< numberOfDoctors; doctor++){
-            if(severity == -1 ){
+            if(severity == -1){
                 if(doctorAttendingPatient[doctor]==null){
                     return doctor;
                 }
@@ -140,10 +147,10 @@ class HospitalScheduler{
     }
 }
 
-// Scheduler class
-public class Scheduler{
+// HospitalEmergencyWardSimulation
+public class HospitalEmergencyWardSimulation {
     public static void main(String[] args) {
-        HospitalScheduler scheduler =  new Hospital Scheduler(1);
+        HospitalScheduler scheduler = new HospitalScheduler(1);
         scheduler.addNewPatient(new Patient(1, 1, 5));
         scheduler.addNewPatient(new Patient(2, 1, 2));
         scheduler.addNewPatient(new Patient(3, 0, 10));
